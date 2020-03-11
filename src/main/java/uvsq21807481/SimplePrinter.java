5a3b4c09fdebe033e3d098f0dd3104aad8bde442
21807481 <hadrien.herubel@ens.uvsq.fr>
2020-03-11 10:27:07 +0100

    Mise a jour pom

--- src/main/java/uvsq21807481/SimplePrinter.java
+++ src/main/java/uvsq21807481/SimplePrinter.java
@@ -13,12 +13,12 @@ public class SimplePrinter implements Printer{
     public void print() { /* print a document */}
 
     @Override
-    public void scan() { thrown new UnsupportedOperationException(); }
+    public void scan() { throw new UnsupportedOperationException(); }
 
     @Override
-    public void copy() { thrown new UnsupportedOperationException(); }
+    public void copy() { throw new UnsupportedOperationException(); }
 
     @Override
-    public void fax() { thrown new UnsupportedOperationException(); }
+    public void fax() { throw new UnsupportedOperationException(); }
 
 }
